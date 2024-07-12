b33219f2aef29ed0b55f54c432f5bfcbc48cb0ed
permito <penciller@disroot.org>
2024-07-12 19:01:20 +0200

    Add a primitive randomization quality test

--- tests/randshow_test.cpp
+++ tests/randshow_test.cpp
@@ -1,11 +1,36 @@
-#include <catch2/catch.hpp>
 #include "../include/randshow.hpp"
+
+#include <catch2/catch.hpp>
+#include <cstddef>
+#include <iostream>
+#include <unordered_map>
 
 TEST_CASE("Sanity check") {
     REQUIRE(0 == 0);
 }
 
-TEST_CASE("Lcg engine randomization quality") {
-    auto engine = randshow::lcg::LCG();
-    REQUIRE(engine.coin_flip() <= 1);
+bool test_rng(const randshow::RNG& rng, size_t number_range, int numbers_per_bucket, double error_threshold) {
+    auto map = std::unordered_map<int, int>();
+    for (size_t i = 0; i < number_range * numbers_per_bucket; i++) {
+        map[rng.next32_range(number_range)] += 1;
+    }
+
+    const auto acceptable_error = numbers_per_bucket * error_threshold;
+    for (const auto [k, v] : map) {
+        std::cout << k << " " << v << " " << numbers_per_bucket - acceptable_error << " "
+                  << numbers_per_bucket + acceptable_error << "\n";
+
+        if (v < numbers_per_bucket - acceptable_error) return false;
+        if (v > numbers_per_bucket + acceptable_error) return false;
+    }
+    return true;
+}
+
+TEST_CASE("Randomization quality") {
+    constexpr auto number_range = 2000;
+    constexpr auto precision = 1200;
+    constexpr auto error_threshold = 0.1;
+
+    CHECK(test_rng(randshow::lcg::LCG(), number_range, precision, error_threshold));
+    CHECK(test_rng(randshow::pcg::PCG(), number_range, precision, error_threshold));
 }
