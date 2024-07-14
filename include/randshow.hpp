dd660d0a08aa5a1d850eba3e2e5a4f355998896c
permito <penciller@disroot.org>
2024-07-14 18:23:14 +0200

    Add Shuffle & Sample, update Style to Google

--- include/randshow.hpp
+++ include/randshow.hpp
@@ -1,71 +1,112 @@
 #pragma once
 
+#include <algorithm>
 #include <chrono>
 #include <cmath>
+#include <cstddef>
 #include <cstdint>
+#include <numeric>
+#include <utility>
+#include <vector>
 
 namespace randshow {
 class RNG {
    public:
     // Next random 32-bit unsigned int.
-    [[nodiscard]] virtual uint32_t next32() const {
-        return advance32();
-    }
+    [[nodiscard]] virtual uint32_t Next32() const { return Advance32(); }
 
-    // Next random 32-bit unsigned int from uniform distribution in [0, n) range.
-    [[nodiscard]] virtual uint32_t next32(uint32_t n) const {
-        return next32() % n;
+    // Next random 32-bit unsigned int from uniform distribution in [0, n)
+    // range.
+    [[nodiscard]] virtual uint32_t Next32(uint32_t n) const {
+        return Next32() % n;
     }
 
     // Next random 32-bit signed int from uniform distribution in [a, b) range.
-    [[nodiscard]] virtual int32_t next32(int32_t a, int32_t b) const {
-        return b > a ? next32() % (b - a) + a : a;
+    [[nodiscard]] virtual int32_t Next32(int32_t a, int32_t b) const {
+        return b > a ? Next32() % (b - a) + a : a;
     }
 
     // Next random 64-bit unsigned int.
-    [[nodiscard]] virtual uint64_t next64() const {
-        return advance64();
+    [[nodiscard]] virtual uint64_t Next64() const { return Advance64(); }
+
+    // Next random 64-bit unsigned int from uniform distribution in [0, n)
+    // range.
+    [[nodiscard]] virtual uint64_t Next64(uint64_t n) const {
+        return Next64() % n;
     }
 
-    // Next random 64-bit unsigned int from uniform distribution in [0, n) range.
-    [[nodiscard]] virtual uint64_t next64(uint64_t n) const {
-        return next64() % n;
-    }
-
-    // Next random 64-bit unsigned int from uniform distribution in [a, b) range.
-    [[nodiscard]] virtual int64_t next64(int64_t a, int64_t b) const {
-        return b > a ? next64() % (b - a) + a : a;
+    // Next random 64-bit unsigned int from uniform distribution in [a, b)
+    // range.
+    [[nodiscard]] virtual int64_t Next64(int64_t a, int64_t b) const {
+        return b > a ? Next64() % (b - a) + a : a;
     }
 
     // Next floating point number from standard uniform distribution i.e. (0, 1)
     // range.
-    [[nodiscard]] virtual double nextfp() const {
-        return static_cast<double>(next32()) / UINT32_MAX;
+    [[nodiscard]] virtual double NextFloating() const {
+        return static_cast<double>(Next32()) / UINT32_MAX;
     }
 
     // Floating point uniform distribution in [a, b) range.
-    [[nodiscard]] virtual double nextfp(double a, double b) const {
-        return nextfp() * (b - a) + a;
+    [[nodiscard]] virtual double NextFloating(double a, double b) const {
+        return NextFloating() * (b - a) + a;
     }
 
     // Balanced coin flip with 50% chance of heads and tails.
-    [[nodiscard]] virtual bool heads() const {
-        return nextfp() < 0.5;
-    }
+    [[nodiscard]] virtual bool Heads() const { return NextFloating() < 0.5; }
 
     // Weighted coin flip. Parameter weight must be in (0, 1) range.
     // Weight smaller or equal than 0 will always yield false and
     // bigger or equal to 1 always yielding true.
-    [[nodiscard]] virtual bool heads(double weight) const {
-        return nextfp() < weight;
+    [[nodiscard]] virtual bool Heads(double weight) const {
+        return NextFloating() < weight;
     }
 
    private:
-    [[nodiscard]] virtual uint32_t advance32() const = 0;
-    [[nodiscard]] virtual uint64_t advance64() const = 0;
+    [[nodiscard]] virtual uint32_t Advance32() const = 0;
+    [[nodiscard]] virtual uint64_t Advance64() const = 0;
 };
 
-namespace lcg {
+// Classic Fisher-Yates shuffle algorithm implementation
+// Link: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+template <class Iterator>
+void Shuffle(const RNG& rng, Iterator begin, Iterator end) {
+    if (begin >= end) return;
+
+    const size_t length = end - begin;
+    for (size_t i = 0; i != length - 2; i++) {
+        const auto j = rng.Next64(i, length);
+        std::swap(*(begin + i), *(begin + j));
+    }
+}
+
+// Reservoir Sampling algorithm implementation, currently Algoritm R variant
+// Link: https://en.wikipedia.org/wiki/Reservoir_sampling
+template <class Iterator>
+std::vector<Iterator> Sample(const RNG& rng, Iterator begin, Iterator end,
+                             size_t n) {
+    const size_t size = end - begin;
+
+    if (size <= n) {
+        std::vector<Iterator> reservoir(size);
+        std::iota(reservoir.begin(), reservoir.end(), begin);
+        Shuffle(rng, reservoir.begin(), reservoir.end());
+        return reservoir;
+    }
+
+    std::vector<Iterator> reservoir(n);
+    std::iota(reservoir.begin(), reservoir.end(), begin);
+
+    for (size_t i = n + 1; i <= size; i++) {
+        const auto j = rng.Next64(i);
+        if (j < n) {
+            reservoir[j] = begin + i;
+        }
+    }
+
+    return reservoir;
+}
+
 // LCG or Linear Congruential Generator has very low footprint and is very fast.
 // It has m_modulo limit of generating pseudorandom numbers. LCGs are suitable
 // for games or other trivial use-cases, but shouldn't be used for work
@@ -73,17 +114,16 @@ namespace lcg {
 //
 // This LCG implementation requires 32 bytes of memory per instance.
 class LCG : public RNG {
-   public:
-    uint32_t advance32() const override {
-        return advance64();
-    }
+   private:
+    uint32_t Advance32() const override { return Advance64(); }
 
-    uint64_t advance64() const override {
-        auto x = m_State;
+    uint64_t Advance64() const override {
+        const auto x = m_State;
         m_State = (m_Mul * m_State + m_Inc) % m_Mod;
         return x;
     }
 
+   public:
     // Creates a new LCG engine with a, c, m parameters equal to the default
     // engine. Seed is current time.
     LCG() = default;
@@ -102,48 +142,39 @@ class LCG : public RNG {
         : m_State(seed), m_Mul(multiplier), m_Inc(increment), m_Mod(modulo) {}
 
     // Getter for state value.
-    [[nodiscard]] uint64_t state() const noexcept {
-        return m_State;
-    }
+    [[nodiscard]] uint64_t GetState() const noexcept { return m_State; }
 
     // Getter for state multiplier.
-    [[nodiscard]] uint64_t mul() const noexcept {
-        return m_Mul;
-    }
+    [[nodiscard]] uint64_t GetMul() const noexcept { return m_Mul; }
 
     // Getter for multiplied state increment.
-    [[nodiscard]] uint64_t inc() const noexcept {
-        return m_Inc;
-    }
+    [[nodiscard]] uint64_t GetInc() const noexcept { return m_Inc; }
 
     // Getter for incremented state modulo.
-    [[nodiscard]] uint64_t mod() const noexcept {
-        return m_Mod;
-    }
+    [[nodiscard]] uint64_t GetMod() const noexcept { return m_Mod; }
 
    private:
-    mutable uint64_t m_State = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    mutable uint64_t m_State =
+        std::chrono::high_resolution_clock::now().time_since_epoch().count();
     uint64_t m_Mul = 6458928179451363983ULL;
     uint64_t m_Inc = 0;
     uint64_t m_Mod = ((1ULL << 63ULL) - 25ULL);
 };
-}  // namespace lcg
 
-namespace pcg {
 class PCG : public RNG {
    private:
     // (high_bits XOR low_bits) of x.
-    static uint64_t xorshift32(uint64_t x) {
+    static uint64_t Xorshift32(uint64_t x) {
         auto high_bits = static_cast<uint32_t>(x >> 32U);
         auto low_bits = static_cast<uint32_t>(x);
         return high_bits ^ low_bits;
     }
 
-    static uint32_t rotr32(uint64_t x, uint32_t rot) {
+    static uint32_t Rotr32(uint64_t x, uint32_t rot) {
         return (x >> rot) | (x << ((-rot) & 31U));
     }
 
-    uint64_t advance() const {
+    uint64_t Advance() const {
         auto x = m_State;
         m_State = m_Mul * m_State + m_Inc;
         return x;
@@ -151,29 +182,30 @@ class PCG : public RNG {
 
    public:
     // PCG-XSH-RR
-    uint32_t advance32() const override {
-        uint64_t x = advance();
+    uint32_t Advance32() const override {
+        uint64_t x = Advance();
         auto rotation = static_cast<uint32_t>(x >> 59U);
 
-        x = xorshift32(x);
-        x = rotr32(x, rotation);
+        x = Xorshift32(x);
+        x = Rotr32(x, rotation);
         return x;
     }
 
     // PCG-XSH-RR-RR
-    uint64_t advance64() const override {
-        uint64_t x = advance();
-        auto rotation = static_cast<uint32_t>(x >> 59U);
+    uint64_t Advance64() const override {
+        uint64_t x = Advance();
+        uint32_t rotation = x >> 59U;
 
         // XSH
-        auto high_bits = static_cast<uint32_t>(x >> 32U);
-        auto low_bits = static_cast<uint32_t>(x);
+        uint32_t high_bits = x >> 32U;
+        uint32_t low_bits = x;
         x = low_bits ^ high_bits;
 
         // RR-RR
-        auto x_low = rotr32(x, rotation);
-        auto x_high = rotr32(high_bits, x & 31U);
-        x = (static_cast<uint64_t>(x_high) << 32U) | x_low;
+        uint32_t x_low = Rotr32(x, rotation);
+        uint64_t x_high = Rotr32(high_bits, x & 31U);
+        x = (x_high << 32U) | x_low;
+
         return x;
     }
 
@@ -181,32 +213,25 @@ class PCG : public RNG {
 
     explicit PCG(uint64_t seed) : m_State(seed) {}
 
-    PCG(uint64_t multiplier, uint64_t increment) : m_Mul(multiplier), m_Inc(increment) {}
+    PCG(uint64_t multiplier, uint64_t increment)
+        : m_Mul(multiplier), m_Inc(increment) {}
 
     PCG(uint64_t seed, uint64_t multiplier, uint64_t increment)
         : m_State(seed), m_Mul(multiplier), m_Inc(increment) {}
 
     // Getter for state value.
-    [[nodiscard]] uint64_t state() const {
-        return m_State;
-    }
+    [[nodiscard]] uint64_t GetState() const { return m_State; }
 
     // Getter for state multiplier.
-    [[nodiscard]] uint64_t mul() const {
-        return m_Mul;
-    }
+    [[nodiscard]] uint64_t GetMul() const { return m_Mul; }
 
     // Getter for multiplied state increment.
-    [[nodiscard]] uint64_t inc() const {
-        return m_Inc;
-    }
+    [[nodiscard]] uint64_t GetInc() const { return m_Inc; }
 
    private:
-    mutable uint64_t m_State = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    mutable uint64_t m_State =
+        std::chrono::high_resolution_clock::now().time_since_epoch().count();
     const uint64_t m_Mul = 6364136223846793005UL;
     const uint64_t m_Inc = 1442695040888963407UL;
 };
-
-static const PCG default_engine = PCG();
-}  // namespace pcg
 }  // namespace randshow
--- tests/randshow_test.cpp
+++ tests/randshow_test.cpp
@@ -5,19 +5,19 @@
 #include <iostream>
 #include <unordered_map>
 
-TEST_CASE("Sanity check") {
-    REQUIRE(0 == 0);
-}
+TEST_CASE("Sanity check") { REQUIRE(0 == 0); }
 
-bool test_rng(const randshow::RNG& rng, size_t number_range, int numbers_per_bucket, double error_threshold) {
+bool test_rng(const randshow::RNG& rng, size_t number_range,
+              int numbers_per_bucket, double error_threshold) {
     auto map = std::unordered_map<int, int>();
     for (size_t i = 0; i < number_range * numbers_per_bucket; i++) {
-        map[rng.next32(number_range)] += 1;
+        map[rng.Next32(number_range)] += 1;
     }
 
     const auto acceptable_error = numbers_per_bucket * error_threshold;
     for (const auto [k, v] : map) {
-        std::cout << k << " " << v << " " << numbers_per_bucket - acceptable_error << " "
+        std::cout << k << " " << v << " "
+                  << numbers_per_bucket - acceptable_error << " "
                   << numbers_per_bucket + acceptable_error << "\n";
 
         if (v < numbers_per_bucket - acceptable_error) return false;
@@ -31,6 +31,6 @@ TEST_CASE("Randomization quality") {
     constexpr auto precision = 1200;
     constexpr auto error_threshold = 0.2;
 
-    CHECK(test_rng(randshow::lcg::LCG(), number_range, precision, error_threshold));
-    CHECK(test_rng(randshow::pcg::PCG(), number_range, precision, error_threshold));
+    CHECK(test_rng(randshow::LCG(), number_range, precision, error_threshold));
+    CHECK(test_rng(randshow::PCG(), number_range, precision, error_threshold));
 }
